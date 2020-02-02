1d626d0d4b0498f841efc4b9570e73015f62bd5b
puneet <puneet@192.168.0.4>
2020-02-02 20:46:55 +0530

    new programs

---
+++ practise/src/practise/DeleteAlternateChar.java
@@ -0,0 +1,45 @@
+package practise;
+
+import java.util.Scanner;
+
+public class DeleteAlternateChar {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter the string"); 
+         Scanner scr=new Scanner(System.in);
+         String s=scr.nextLine();
+        
+        
+        int result=0;
+        
+        for(int i=0;i<s.length()-1;i++) {
+        if(s.charAt(i)==s.charAt(i+1)) {
+            result++;
+            
+        }
+            
+            
+        }
+        System.out.println(result); 
+
+		
+		
+		
+		
+		
+		/*
+		 * String s="AAAABBBB";
+		 * 
+		 * char[] c=s.toCharArray(); int result=0;
+		 * 
+		 * for(int i=0;i<c.length-1;i++) { if(c[i]==c[i+1]) { result=result+1;
+		 * 
+		 * }
+		 * 
+		 * 
+		 * } System.out.println(result);
+		 * 
+		 */	}
+
+}
---
+++ practise/src/practise/DuplicateChar.java
@@ -0,0 +1,92 @@
+package practise;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class DuplicateChar {
+
+	public static void main(String[] args) {
+		
+		String s="amazonautomation";
+		char[] c=s.toCharArray();
+		int size=c.length;
+		LinkedHashMap<Character,Integer> se=new LinkedHashMap<>();
+		int i=0;
+		while(i!=size) {
+			
+			if(se.containsKey(c[i])==false)
+			{
+				se.put(c[i], 1);
+				
+				
+				
+			}else {
+				int oldval=se.get(c[i]);
+				int newval=oldval+1;
+				se.put(c[i], newval);
+			}
+			
+			
+			i++;
+			
+		}
+		
+		
+		
+		Set<Entry<Character,Integer>>  set=se.entrySet();
+		
+		
+		for(Entry<Character,Integer> data: set) {
+			
+			if(data.getValue()>1) {
+				System.out.println(data.getKey());
+				System.out.println(data.getValue());
+			}
+			
+			
+			
+			
+		}
+		
+		
+		
+		/*
+		 * Set<Character> se= new HashSet<>();
+		 * 
+		 * for(char v:c) { if(se.add(v)==false) { System.out.println(v);
+		 * 
+		 * 
+		 * }
+		 * 
+		 * 
+		 * }
+		 */
+		
+		
+		
+		 
+		
+		
+		
+		/*for(int i=0;i<s.length()-1;i++)
+		{
+			for(int j=i+1;j<s.length();j++) {
+				
+				if(c[i]==c[j] && i!=j) {
+					
+					System.out.println(c[j]);    
+				}
+				
+				
+			}*/
+			
+			
+		}
+		
+
+	}
+
+
